f171a16bcc8fa7e65db9ba8278171feaf127e993
Agustin-Zabalgoitia <aguszabalgoitia@gmail.com>
2025-08-07 14:26:48 -0300

    SBA-6 Added validation to login endpoint

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { authController } from "../controllers/authController";
+import { loginValidation } from "../validations/authValidation";
 
 export const authRouter = Router();
 
-authRouter.post("/login", authController.login);
+authRouter.post("/login", loginValidation, authController.login);
 authRouter.post("/logout", authController.logout);
